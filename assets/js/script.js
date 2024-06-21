c0264e2427277b694803c88347384b5118eb2904
maruf1996 <maruf3813@gmail.com>
2024-06-22 01:32:01 +0600

    added-magnific-popup-and-responsive-header

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(function () {
+  // Mobile Menu
+  $(".mobile-menu-toggle").click(function () {
+    $(".mobile-menu").slideToggle();
+  });
+  $(".mobile-menu nav ul li.has-submenu").click(function () {
+    $(this).children("ul").slideToggle();
+  });
+  // magnific popup
+  $(".video-btn a").magnificPopup({
+    type: "iframe",
+    iframe: {
+      patterns: {
+        youtube: {
+          index: "youtube.com/",
+          src: "https://www.youtube.com/embed/%id%?autoplay=1",
+        },
+      },
+    },
+  });
+});
